c9bcc1850866866ed9f16f099a944ebc693a1d6d
Ali CHERIFI <cherifiali@outlook.fr>
2018-12-01 12:43:23 +0100

    Branche front

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatCardModule} from '@angular/material/card';
+import { MatToolbarModule, MatButtonModule, MatIconModule, MatCardModule } from '@angular/material/';
+import {HttpClientModule} from '@angular/common/http';
+import { NavbarComponent } from './navbar/navbar.component';
+import { MoviecardComponent } from './moviecard/moviecard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    MoviecardComponent,
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    HttpClientModule,
     MatCardModule
   ],
   providers: [],
