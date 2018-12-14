a17ae682dc3ac5594eb341e4db9010337e00d43e
cfontenay <clementfontenay@gmail.com>
2018-12-14 12:37:01 +0100

    amélioration de la fonction de tri et surtout décision d'arrêter d'essayer de faire du fonctionnelle (parce que le js c'est de la merde)

--- webApp/src/services/SortService.ts
+++ webApp/src/services/SortService.ts
@@ -2,56 +2,81 @@ import { IParam } from './objects/sortParameters/IParam';
 import { Movie } from './objects/Movie';
 type HandlerObserverSortService = (origin: SortService) => any;
 export class SortService {
-  private sortParameters: Map<number, IParam[]>;
+  private sortParameters: Map<number, Map<String, IParam[]>>;
   private sortedParametersHasChanged: Map<number, boolean>;
   private rawMovies: Map<number, Movie[]>;
   private rawDataHasChanged: Map<number, boolean>;
   private handlersObservers: HandlerObserverSortService[];
 
   constructor() {
-    this.sortParameters = new Map<number, IParam[]>();
+    this.sortParameters = new Map<number, Map<String, IParam[]>>();
     this.sortedParametersHasChanged = new Map<number, boolean>();
     this.rawMovies = new Map<number, Movie[]>();
     this.rawDataHasChanged = new Map<number, boolean>();
     this.handlersObservers = [];
   }
   async addParam(idPage: number, param: IParam) {
-    await console.log(await this.sortParameters.get(idPage));
      if (await this.sortParameters.get(idPage) === undefined) {
-       await this.sortParameters.set(idPage, []);
+       await this.sortParameters.set(idPage, await new Map<String, IParam[]>());
      }
-     const index = await (await this.sortParameters.get(idPage)).findIndex(x => x.equals(param));
+     if (await (await this.sortParameters.get(idPage)).get(param.getKey()) === undefined) {
+      await this.sortParameters.get(idPage).set(await param.getKey(), []);
+     }
+     const index = (await (await this.sortParameters.get(idPage)).get(await param.getKey())).findIndex(x => x.equals(param));
      if (index === -1) {
-       await this.sortParameters.get(idPage).push(param);
+       await (await (await this.sortParameters.get(idPage)).get(await param.getKey())).push(await param);
        await this.setTrueToSortedParametersChanged(idPage);
      }
   }
   async removeParam(idPage: number, param: IParam) {
-    if (await this.sortParameters.get(idPage) !== undefined) {
-      const index = await this.sortParameters.get(idPage).findIndex(x => x.equals(param));
+    if (await this.sortParameters.get(idPage) !== undefined &&
+    await this.sortParameters.get(idPage).get(await param.getKey()) !== undefined) {
+      const index = await (await (await (this.sortParameters.get(idPage)).get(await param.getKey()))).findIndex(x => x.equals(param));
       if (index !== -1) {
-        await this.sortParameters.set(idPage, await (await this.sortParameters.get(idPage).
-        filter(x => !x.equals(param))));
+        await this.sortParameters.get(idPage).set(param.getKey(), await (await this.sortParameters.get(idPage).
+        get(await param.getKey())).filter(x => !x.equals(param)));
         await this.setTrueToSortedParametersChanged(idPage);
       }
     }
   }
   async getParams(idPage: number): Promise<IParam[]> {
-    return await this.sortParameters.get(idPage);
+    const tempoParams = [];
+    const mapParams = await this.sortParameters.get(idPage);
+    await Array.from(await mapParams.keys()).forEach( async k => {
+      await tempoParams.push(mapParams.get(k));
+    });
+    return tempoParams;
   }
+  /**
+   * Pour la même clé de paramètres c'est un "ou" pour les films et quand c'est une autre clé c'est un "et"
+   */
   async getSortedMovies(idPage: number): Promise<Movie[]> {
-    const rawMovies = await this.rawMovies.get(idPage);
-    const sortedMovies = [];
-    const params = (await this.sortParameters.get(idPage));
-    if (params === undefined || params.length === 0) {
-      return await this.rawMovies.get(idPage);
-    }
-    for (let i = 0; i < params.length; ++i) {
-      for (let j = 0; j < rawMovies.length; ++j) {
-        if (await rawMovies[j].contains(params[i])  && await sortedMovies.findIndex(x => x === rawMovies[j]) === -1) {
-          await sortedMovies.push(rawMovies[j]);
+    const functionRespectParams = async (movie: Movie, params: IParam[]): Promise<boolean> => {
+      let res = params.length === 0;
+      for (let i = 0; i < params.length; ++i) {
+        if (await movie.contains(params[i])) {
+          res = true;
+          break;
         }
       }
+      return res;
+    };
+    const rawMovies = await this.rawMovies.get(idPage);
+    const mapParams = await this.sortParameters.get(idPage);
+    if (mapParams === undefined) {
+      return rawMovies;
+    }
+    let sortedMovies = rawMovies;
+    const keys = await Array.from(await mapParams.keys());
+    for (let i = 0; i < keys.length; ++i) {
+      const tempoSortedMovies = [];
+      for (let j = 0; j < sortedMovies.length; ++j) {
+        const params = await mapParams.get(keys[i]);
+        if (await functionRespectParams(sortedMovies[j], params) === true) {
+          await tempoSortedMovies.push(sortedMovies[j]);
+        }
+      }
+      sortedMovies = tempoSortedMovies;
     }
     return sortedMovies;
   }
